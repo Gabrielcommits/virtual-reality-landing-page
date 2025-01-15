afe4b5c964890ef8c289e7287e67d1b7c0910310
gabriel maldonado <curtainsopened82@gmail.com>
2025-01-15 14:14:16 -0600

    Make testimonials p and name bold and white

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -10,7 +10,7 @@ const Testimonials = () => {
         {testimonials.map((testimonial, index) => (
           <div key={index} className="w-full sm:w-1/2 lg:w-1/3 px-4 py-2">
             <div className="bg-neutral-900 rounded-md p-6 text-md border border-neutral-800 font-thin">
-              <p>{testimonial.text}</p>
+              <p className="text-white-300 font-bold">{testimonial.text}</p>
               <div className="flex mt-8 items-start">
                 <img
                   className="w-12 h-12 mr-6 rounded-full border border-neutral-300"
@@ -18,7 +18,7 @@ const Testimonials = () => {
                   alt=""
                 />
                 <div>
-                  <h6>{testimonial.user}</h6>
+                  <h6 className="text-white-300 font-bold">{testimonial.user}</h6>
                   <span className="text-sm font-normal italic text-neutral-600">
                     {testimonial.company}
                   </span>
